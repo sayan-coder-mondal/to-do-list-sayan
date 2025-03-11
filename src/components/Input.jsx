d06b02c7549d8ed5f6988cbddfc2aee510d86c8b
sayan-coder-mondal <sayanmondal132@gmail.com>
2025-03-12 00:20:58 +0530

    Third commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -19,7 +19,7 @@ function Input(props) {
     return (
         <>
             <div style={{ display: 'flex', flexDirection:'column', justifyContent: 'center', width: '80vw', margin: '20px auto' }}>
-                <input type="text" onChange={(e) => setText(e.target.value)} style={{ width: '60vw', fontSize: '20px', padding: '10px' ,margin:'auto'}} />
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} style={{ width: '60vw', fontSize: '20px', padding: '10px' ,margin:'auto'}} />
                 <div style={{margin:'auto'}}>
                     <button onClick={addToLocal} style={{ width: '120px', backgroundColor: 'blue', color: 'white', margin: '10px' }}>Add</button>
                     <button onClick={handleAllDelete} style={{ width: '120px', backgroundColor: 'red', color: 'white', margin: '10px' }}>Clear All</button>
